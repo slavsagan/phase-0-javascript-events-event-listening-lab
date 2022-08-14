9d05b5910af9d186128e02f951c6ae00cfe26b90
Slav <itiksolution@gmail.com>
2022-08-14 15:14:06 +1000

    asignment complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-function addingEventListener() {
+const input = document.getElementById('button')
+
+const addingEventListener = function() {
+  input.addEventListener('click', function() {
+    alert ('I was clicked')
+  })
 }
+
+/** OPTIONAL */
+// const addingEventListener = function() {
+//   const input = document.getElementById('button')
+//   input.addEventListener('click', function() {
+//     alert ('I was clicked')
+//   })
+// }
